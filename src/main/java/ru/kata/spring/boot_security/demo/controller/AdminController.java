c694d43553413cebde737409ed94a8120dd865f3
basniniv <155753031+basniniv@users.noreply.github.com>
2024-05-02 23:29:34 +0400

    add modal window

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -5,17 +5,21 @@ import java.util.List;
 import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.repository.RoleRepository;
 import ru.kata.spring.boot_security.demo.service.UserDetService;
 
 @Controller
-
+@RequestMapping("/admin")
 public class AdminController {
 
     private final UserDetService userDetService;
@@ -27,16 +31,20 @@ public class AdminController {
     }
 
     //===================/show all users/===================
-    @GetMapping("/admin")
+    @GetMapping
     public String getUsers(Model model, Principal principal) {
-        User user = new User();
         User currentUser = userDetService.findByUsername(principal.getName());
-        List<User> usersList = userDetService.allUsers();
-        List<Role> roles = roleRepository.findAll();
-        model.addAttribute("user", user);
         model.addAttribute("currentUser", currentUser);
+
+        List<User> usersList = userDetService.allUsers();
         model.addAttribute("users", usersList);
+
+        List<Role> roles = roleRepository.findAll();
         model.addAttribute("allRoles", roles);
+
+        User user = new User();
+        model.addAttribute("user", user);
+
         if(currentUser == null){
             return "redirect:/login";
         }
@@ -44,40 +52,36 @@ public class AdminController {
     }
     //===================/create/====================
 
-    @GetMapping("/addUser")
-    public String addUserForm(Model model) {
-        User user = new User();
-        model.addAttribute("user", user);
-        List<Role> roles = roleRepository.findAll();
-        model.addAttribute("allRoles", roles);
-        return "admin/addUser";
-    }
-
     @PostMapping("/addUserToDB")
     public String addUser(@ModelAttribute("user") @Valid User user) {
         userDetService.saveUser(user);
-        return "redirect:/admin";
+
+        return "redirect:/admin/";
     }
 
     //===================//remove//====================
-    @PostMapping("/deleteUser/{id}")
+    @DeleteMapping("/{id}")
     public String deleteUser(@PathVariable("id") long id) {
         userDetService.deleteUser(id);
 
-        return "redirect:/admin";
+        return "redirect:/admin/";
+
     }
 
     //*****************************************//update//********************************************
-    @GetMapping("/edit/{id}")
-    public String updateUserForm(Model model, @PathVariable("id") Integer id) {
-        model.addAttribute("user", userDetService.getUser(id));
-        List<Role> roles = roleRepository.findAll();
-        model.addAttribute("allRoles", roles);
-        return "admin/update";
-    }
+//    @PatchMapping("/edit/{id}")
+//    public String updateUserForm(Model model, @PathVariable("id") Integer id) {
+//        model.addAttribute("user", userDetService.getUser(id));
+//        List<Role> roles = roleRepository.findAll();
+//        model.addAttribute("allRoles", roles);
+//        return "admin/update";
+//    }
 
-    @PostMapping("/updateUser")
-    public String updateUser(@ModelAttribute("user") User user) {
+    @PatchMapping("/{id}")
+    public String updateUser(@ModelAttribute("user") @Valid User user, BindingResult bindingResult) {
+//        if (bindingResult.hasErrors()){
+//            return "/{id}";
+//        }
         userDetService.updateUser(user);
         return "redirect:/admin";
     }
